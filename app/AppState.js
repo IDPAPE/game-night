dabd31d0ec8b33bf9c8457e644c8f8cb4bf6eb22
isaiah <isaiahpape@gmail.com>
2024-03-25 14:53:21 -0600

    model, appstate, some html created

--- app/AppState.js
+++ app/AppState.js
@@ -1,3 +1,4 @@
+import { Player } from './models/Players.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
@@ -5,6 +6,21 @@ class ObservableAppState extends EventEmitter {
 
   /**@type {import('./models/Example.js').Example[]} */
   examples = []
+  players = [
+    new Player(`Amanda`, 0),
+    new Player(`George`, 0),
+    new Player(`John`, 0)
+  ]
+
+
+
+
+
+
+
 }
 
 export const AppState = createObservableProxy(new ObservableAppState())
+
+
+console.log(AppState)
---
+++ app/models/Players.js
@@ -0,0 +1,6 @@
+export class Player {
+    constructor(name, points) {
+        this.name = name
+        this.points = points
+    }
+}
